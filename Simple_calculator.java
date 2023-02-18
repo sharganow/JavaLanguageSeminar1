4c1e263fc7b681c452d329d06629aba56b6f8d88
sharganow <sharganow@gmail.com>
2023-02-18 17:55:48 +0500

    Калькулятор ужастно примитивный заработал

--- Simple_calculator.java
+++ Simple_calculator.java
@@ -1,17 +1,18 @@
 import java.lang.Double;
+import java.lang.Integer;
+import java.util.Scanner;
 public class Simple_calculator {
     public static void main(String[] args) {
         System.out.println("Напишите задание для калькулятора в виде \"12 * 3 = \" без кавычек: ");
- //       Scanner iScanner = new Scanner(System.in);
-        String user_task = "-12 - 2.456 =";
-                //iScanner.toString();
-//        iScanner.close();
+        Scanner iScanner = new Scanner(System.in);
+        String user_task = iScanner.next();
+        iScanner.close();
         System.out.printf(user_task);
         System.out.println(execute_task(user_task));
     }
-    public static double execute_task(String user_task) {
-        double arg1 = 0;
-        double arg2 = 0;
+    public static int execute_task(String user_task) {
+        int arg1 = 0;
+        int arg2 = 0;
         String operand = "Nothing";
         String state = "Найти первый аргумент";
         StringBuilder arg = new StringBuilder();
@@ -21,78 +22,96 @@ public class Simple_calculator {
                     switch (user_task.charAt(i)) {
                         case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9': {
                             arg.append(user_task.charAt(i));
-                        }
+                        }break;
                         case ',', '.': {
                             arg.append('.');
-                        }
+                        }break;
                         case ' ':{
-                            arg1 = Double.parseDouble(arg.toString());
+                            arg1 = Integer.decode(arg.toString());
                             state = "Определить операнд";
-                            arg.delete(0, arg.length()-1);
-                        }
+                            arg.delete(0, arg.length());
+                        }break;
                         case '-', '+', '*', ':': {
                             if (user_task.charAt(i) == '-' && arg.length() == 0) {
                                 arg.append(user_task.charAt(i));
                             }
                             else {
-                                arg1 = Double.parseDouble(arg.toString());
+                                arg1 = Integer.decode(arg.toString());
                                 operand = Character.toString(user_task.charAt(i));
                                 state = "Найти второй аргумент";
-                                arg.delete(0, arg.length()-1);
+                                arg.delete(0, arg.length());
                             }
-                        }
+                        }break;
                     }
-                }
+                }break;
                 case "Определить операнд":{
                     switch (user_task.charAt(i)) {
                         case '-', '+', '*', ':': {
                             operand = Character.toString(user_task.charAt(i));
                             state = "Найти второй аргумент";
-                        }
+                        }break;
                     }
-                }
+                }break;
                 case "Найти второй аргумент":{
                     switch (user_task.charAt(i)) {
                         case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9': {
                             arg.append(user_task.charAt(i));
-                        }
+                        }break;
                         case ',', '.': {
                             arg.append('.');
-                        }
+                        }break;
                         case ' ':{
-                            arg2 = Double.parseDouble(arg.toString());
-                            state = "Произвести вычисление";
-                        }
+                            if(arg.length() > 0){
+                                arg2 = Integer.decode(arg.toString());
+                                state = "Произвести вычисление";
+                                switch (user_task.charAt(i)) {
+                                    case '=': {
+                                        switch (operand){
+                                            case "-":{
+                                                return arg1 - arg2;
+                                            }
+                                            case "+":{
+                                                return arg1 + arg2;
+                                            }
+                                            case "*":{
+                                                return arg1 * arg2;
+                                            }
+                                            case ":":{
+                                                return arg1 / arg2;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }break;
                         case '-', '=': {
                             if (user_task.charAt(i) == '-' && arg.length() == 0) {
                                 arg.append(user_task.charAt(i));
                             } else {
-                                arg2 = Double.parseDouble(arg.toString());
-
-                            }
-                        }
-                    }
-                }
-                case "Произвести вычисление":{
-                    switch (user_task.charAt(i)) {
-                        case '=': {
-                            switch (operand){
-                                case "-":{
-                                    return arg1 - arg2;
-                                }
-                                case "+":{
-                                    return arg1 + arg2;
-                                }
-                                case "*":{
-                                    return arg1 * arg2;
-                                }
-                                case ":":{
-                                    return arg1 / arg2;
+                                arg2 = Integer.decode(arg.toString());
+                                state = "Произвести вычисление";
+                                switch (user_task.charAt(i)) {
+                                    case '=': {
+                                        switch (operand){
+                                            case "-":{
+                                                return arg1 - arg2;
+                                            }
+                                            case "+":{
+                                                return arg1 + arg2;
+                                            }
+                                            case "*":{
+                                                return arg1 * arg2;
+                                            }
+                                            case ":":{
+                                                return arg1 / arg2;
+                                            }
+                                        }
+                                    }
                                 }
                             }
-                        }
+                        }break;
                     }
-                }
+                }break;
             }
         }
         return -1;
